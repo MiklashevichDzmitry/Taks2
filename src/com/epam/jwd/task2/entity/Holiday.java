1d4e4f9dab78750052045f2f99decccaf8fc9cb1
MiklashevichDzmitry <anfisayou1997@gmail.com>
2022-03-14 18:12:02 +0300

    some changes

---
+++ src/com/epam/jwd/task2/entity/Holiday.java
@@ -0,0 +1,5 @@
+package com.epam.jwd.task2.entity;
+
+public class Holiday {
+
+}
---
+++ src/com/epam/jwd/task2/exception/CustomParseXMLException.java
@@ -0,0 +1,20 @@
+package com.epam.jwd.task2.exception;
+
+public class CustomParseXMLException extends Exception {
+    
+	public CustomParseXMLException() {
+        super();
+    }
+
+    public CustomParseXMLException(String message) {
+        super(message);
+    }
+
+    public CustomParseXMLException(Exception exception) {
+        super(exception);
+    }
+    
+    public CustomParseXMLException(String message, Exception exception) {
+        super(message, exception);
+    }
+}
